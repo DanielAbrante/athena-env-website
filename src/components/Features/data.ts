9bed03849cdd8df253f971f74df8fa149557dfed
Daniel Abrante <danielabrante@protonmail.com>
2024-12-07 15:33:27 -0300

    feat: Add biome and husky to improve code standardization

--- src/components/Features/data.ts
+++ src/components/Features/data.ts
@@ -1,23 +1,27 @@
 export type FeatureItem = {
-    title: string;
-    src: string;
-    description: string;
+	id: number;
+	title: string;
+	src: string;
+	description: string;
 };
 
 export const FeatureList: FeatureItem[] = [
-    {
-        title: 'Easy to Use',
-        src: '/img/features/js.svg',
-        description: 'Simplified JavaScript integration for seamless development.'
-    },
-    {
-        title: 'Fast',
-        src: '/img/features/rocket.svg',
-        description: 'Lightning speed powered by the QuickJS engine'
-    },
-    {
-        title: 'Free and Open Source',
-        src: '/img/features/open_source.svg',
-        description: '100% free and open-source, licensed under GPL-3.0'
-    },
+	{
+		id: 0,
+		title: "Easy to Use",
+		src: "/img/features/js.svg",
+		description: "Simplified JavaScript integration for seamless development.",
+	},
+	{
+		id: 1,
+		title: "Fast",
+		src: "/img/features/rocket.svg",
+		description: "Lightning speed powered by the QuickJS engine",
+	},
+	{
+		id: 2,
+		title: "Free and Open Source",
+		src: "/img/features/open_source.svg",
+		description: "100% free and open-source, licensed under GPL-3.0",
+	},
 ];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,44 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/Features';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import HomepageFeatures from "@site/src/components/Features";
+import Heading from "@theme/Heading";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
 
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+	const { siteConfig } = useDocusaurusContext();
 
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Getting Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className={clsx("hero hero--primary", styles.heroBanner)}>
+			<div className="container">
+				<Heading as="h1" className="hero__title">
+					{siteConfig.title}
+				</Heading>
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
+				<div>
+					<Link
+						className="button button--secondary button--lg"
+						to="/docs/intro"
+					>
+						Getting Started
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+	const { siteConfig } = useDocusaurusContext();
 
-  return (
-    <Layout
-      title='Home'
-      description={`${siteConfig.tagline}`}>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	return (
+		<Layout title="Home" description={`${siteConfig.tagline}`}>
+			<HomepageHeader />
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	);
 }
